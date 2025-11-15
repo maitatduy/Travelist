36cfb4fd8e842bca5f1bdde4fde1ae8a33442380
maitatduy <maitatduy.work@gmail.com>
2025-11-16 00:17:58 +0700

    feat: add dynamic admin path configuration

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const database = require("./config/database.config");
 const app = express();
 const port = 3000;
 
+const adminRoutes = require("./routes/admin/index.route");
+const variableConfig = require("./config/variable.config");
+
 // Kết nối database
 database.connect();
 
@@ -16,6 +19,12 @@ app.set("view engine", "pug");
 
 // Thiết lập thư mục public
 app.use(express.static(path.join(__dirname, "public")));
+
+// Tạo biến toàn cục trong file PUG
+app.locals.pathAdmin = variableConfig.pathAdmin;
+
+// Thiết lập đường dẫn
+app.use(`/${variableConfig.pathAdmin}`, adminRoutes);
 
 app.listen(port, () => {
   console.log(`Website đang chạy ở cổng: ${port}`);
