3b180d595fd81d1d551fe23a67d7f7b79d48cebb
maitatduy <maitatduy.work@gmail.com>
2025-11-16 02:58:06 +0700

    feat: implement user registration validation with joi

--- public/admin/assets/js/account.js
+++ public/admin/assets/js/account.js
@@ -4,118 +4,118 @@ if (loginForm) {
   const validation = new JustValidate("#login-form");
 
   validation
-    .addField("#email", [
-      {
-        rule: "required",
-        errorMessage: "Vui lòng nhập email của bạn!",
-      },
-      {
-        rule: "email",
-        errorMessage: "Email không đúng định dạng!",
-      },
-    ])
-    .addField("#password", [
-      {
-        rule: "required",
-        errorMessage: "Vui lòng nhập mật khẩu!",
-      },
-      {
-        validator: (value) => value.length >= 8,
-        errorMessage: "Mật khẩu phải chứa ít nhất 8 ký tự!",
-      },
-      {
-        validator: (value) => /[A-Z]/.test(value),
-        errorMessage: "Mật khẩu phải chứa ít nhất một chữ cái in hoa!",
-      },
-      {
-        validator: (value) => /[a-z]/.test(value),
-        errorMessage: "Mật khẩu phải chứa ít nhất một chữ cái thường!",
-      },
-      {
-        validator: (value) => /\d/.test(value),
-        errorMessage: "Mật khẩu phải chứa ít nhất một chữ số!",
-      },
-      {
-        validator: (value) => /[@$!%*?&]/.test(value),
-        errorMessage: "Mật khẩu phải chứa ít nhất một ký tự đặc biệt!",
-      },
-    ])
-    .onSuccess((event) => {
-      const email = event.target.email.value;
-      const password = event.target.password.value;
-      const rememberPassword = event.target.rememberPassword.checked;
+    //     .addField("#email", [
+    //       {
+    //         rule: "required",
+    //         errorMessage: "Vui lòng nhập email của bạn!",
+    //       },
+    //       {
+    //         rule: "email",
+    //         errorMessage: "Email không đúng định dạng!",
+    //       },
+    //     ])
+    //     .addField("#password", [
+    //       {
+    //         rule: "required",
+    //         errorMessage: "Vui lòng nhập mật khẩu!",
+    //       },
+    //       {
+    //         validator: (value) => value.length >= 8,
+    //         errorMessage: "Mật khẩu phải chứa ít nhất 8 ký tự!",
+    //       },
+    //       {
+    //         validator: (value) => /[A-Z]/.test(value),
+    //         errorMessage: "Mật khẩu phải chứa ít nhất một chữ cái in hoa!",
+    //       },
+    //       {
+    //         validator: (value) => /[a-z]/.test(value),
+    //         errorMessage: "Mật khẩu phải chứa ít nhất một chữ cái thường!",
+    //       },
+    //       {
+    //         validator: (value) => /\d/.test(value),
+    //         errorMessage: "Mật khẩu phải chứa ít nhất một chữ số!",
+    //       },
+    //       {
+    //         validator: (value) => /[@$!%*?&]/.test(value),
+    //         errorMessage: "Mật khẩu phải chứa ít nhất một ký tự đặc biệt!",
+    //       },
+    //     ])
+    //     .onSuccess((event) => {
+    //       const email = event.target.email.value;
+    //       const password = event.target.password.value;
+    //       const rememberPassword = event.target.rememberPassword.checked;
 
-      console.log(email);
-      console.log(password);
-      console.log(rememberPassword);
-    });
-}
-// End Login Form
+    //       console.log(email);
+    //       console.log(password);
+    //       console.log(rememberPassword);
+    //     });
+    // }
+    // // End Login Form
 
-// Register Form
-const registerForm = document.querySelector("#register-form");
-if (registerForm) {
-  const validation = new JustValidate("#register-form");
+    // // Register Form
+    // const registerForm = document.querySelector("#register-form");
+    // if (registerForm) {
+    //   const validation = new JustValidate("#register-form");
 
-  validation
-    .addField("#fullName", [
-      {
-        rule: "required",
-        errorMessage: "Vui lòng nhập họ tên!",
-      },
-      {
-        rule: "minLength",
-        value: 5,
-        errorMessage: "Họ tên phải có ít nhất 5 ký tự!",
-      },
-      {
-        rule: "maxLength",
-        value: 50,
-        errorMessage: "Họ tên không được vượt quá 50 ký tự!",
-      },
-    ])
-    .addField("#email", [
-      {
-        rule: "required",
-        errorMessage: "Vui lòng nhập email của bạn!",
-      },
-      {
-        rule: "email",
-        errorMessage: "Email không đúng định dạng!",
-      },
-    ])
-    .addField("#password", [
-      {
-        rule: "required",
-        errorMessage: "Vui lòng nhập mật khẩu!",
-      },
-      {
-        validator: (value) => value.length >= 8,
-        errorMessage: "Mật khẩu phải chứa ít nhất 8 ký tự!",
-      },
-      {
-        validator: (value) => /[A-Z]/.test(value),
-        errorMessage: "Mật khẩu phải chứa ít nhất một chữ cái in hoa!",
-      },
-      {
-        validator: (value) => /[a-z]/.test(value),
-        errorMessage: "Mật khẩu phải chứa ít nhất một chữ cái thường!",
-      },
-      {
-        validator: (value) => /\d/.test(value),
-        errorMessage: "Mật khẩu phải chứa ít nhất một chữ số!",
-      },
-      {
-        validator: (value) => /[@$!%*?&]/.test(value),
-        errorMessage: "Mật khẩu phải chứa ít nhất một ký tự đặc biệt!",
-      },
-    ])
-    .addField("#agree", [
-      {
-        rule: "required",
-        errorMessage: "Bạn phải đồng ý với các điều khoản và điều kiện!",
-      },
-    ])
+    //   validation
+    //     .addField("#fullName", [
+    //       {
+    //         rule: "required",
+    //         errorMessage: "Vui lòng nhập họ tên!",
+    //       },
+    //       {
+    //         rule: "minLength",
+    //         value: 5,
+    //         errorMessage: "Họ tên phải có ít nhất 5 ký tự!",
+    //       },
+    //       {
+    //         rule: "maxLength",
+    //         value: 50,
+    //         errorMessage: "Họ tên không được vượt quá 50 ký tự!",
+    //       },
+    //     ])
+    //     .addField("#email", [
+    //       {
+    //         rule: "required",
+    //         errorMessage: "Vui lòng nhập email của bạn!",
+    //       },
+    //       {
+    //         rule: "email",
+    //         errorMessage: "Email không đúng định dạng!",
+    //       },
+    //     ])
+    //     .addField("#password", [
+    //       {
+    //         rule: "required",
+    //         errorMessage: "Vui lòng nhập mật khẩu!",
+    //       },
+    //       {
+    //         validator: (value) => value.length >= 8,
+    //         errorMessage: "Mật khẩu phải chứa ít nhất 8 ký tự!",
+    //       },
+    //       {
+    //         validator: (value) => /[A-Z]/.test(value),
+    //         errorMessage: "Mật khẩu phải chứa ít nhất một chữ cái in hoa!",
+    //       },
+    //       {
+    //         validator: (value) => /[a-z]/.test(value),
+    //         errorMessage: "Mật khẩu phải chứa ít nhất một chữ cái thường!",
+    //       },
+    //       {
+    //         validator: (value) => /\d/.test(value),
+    //         errorMessage: "Mật khẩu phải chứa ít nhất một chữ số!",
+    //       },
+    //       {
+    //         validator: (value) => /[@$!%*?&]/.test(value),
+    //         errorMessage: "Mật khẩu phải chứa ít nhất một ký tự đặc biệt!",
+    //       },
+    //     ])
+    //     .addField("#agree", [
+    //       {
+    //         rule: "required",
+    //         errorMessage: "Bạn phải đồng ý với các điều khoản và điều kiện!",
+    //       },
+    //     ])
     .onSuccess((event) => {
       const fullName = event.target.fullName.value;
       const email = event.target.email.value;
